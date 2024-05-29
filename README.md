814fbd00050b5d311d29cb659dcb9570895c32e2
Siddharth Mundra <smundra@ucsd.edu>
2024-05-29 02:15:35 -0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab8-Starter
+
+Members - Siddharth Mundra
+
+
+Graceful degradation refers to the strategy of building web applications to provide a basic level of user functionality in older browsers whilst still delivering a "fuller" experience in modern environments. This  ensures that even if some advanced features are unsupported, the core functionalities of the web application remain operational in old browsers.
+
+Service workers are scripts that run in the background that provide features like offline support, background sync, and intercepting network requests. 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,5 @@
 // main.js
 
-// CONSTANTS
 const RECIPE_URLS = [
   'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
   'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
@@ -10,28 +9,19 @@ const RECIPE_URLS = [
   'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
 ];
 
-// Run the init() function when the page has loaded
 window.addEventListener('DOMContentLoaded', init);
 
-// Starts the program, all function calls trace back here
 async function init() {
-  // initialize ServiceWorker
   initializeServiceWorker();
-  // Get the recipes from localStorage
   let recipes;
   try {
       recipes = await getRecipes();
   } catch (err) {
       console.error(err);
   }
-  // Add each recipe to the <main> element
   addRecipesToDocument(recipes);
 }
 
-/**
-* Detects if there's a service worker, then loads it and begins the process
-* of installing it and getting it running
-*/
 function initializeServiceWorker() {
   if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
@@ -45,14 +35,6 @@ function initializeServiceWorker() {
   }
 }
 
-/**
-* Reads 'recipes' from localStorage and returns an array of
-* all of the recipes found (parsed, not in string form). If
-* nothing is found in localStorage, network requests are made to all
-* of the URLs in RECIPE_URLs, an array is made from those recipes, that
-* array is saved to localStorage, and then the array is returned.
-* @returns {Array<Object>} An array of recipes found in localStorage
-*/
 async function getRecipes() {
   let storedRecipes = localStorage.getItem('recipes');
   if (storedRecipes) {
@@ -78,22 +60,10 @@ async function getRecipes() {
   }
 }
 
-/**
-* Takes in an array of recipes, converts it to a string, and then
-* saves that string to 'recipes' in localStorage
-* @param {Array<Object>} recipes An array of recipes
-*/
 function saveRecipesToStorage(recipes) {
   localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
-/**
-* Takes in an array of recipes and for each recipe creates a
-* new <recipe-card> element, adds the recipe data to that card
-* using element.data = {...}, and then appends that new recipe
-* to <main>
-* @param {Array<Object>} recipes An array of recipes
-*/
 function addRecipesToDocument(recipes) {
   if (!recipes) return;
   let main = document.querySelector('main');
